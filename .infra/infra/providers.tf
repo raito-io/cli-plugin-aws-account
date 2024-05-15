d5e952056ec6832fdb9e355a0d4d186475a88737
Dieter Wachters <dieter@raito.io>
2024-05-15 11:53:31 +0200

    another try

--- .infra/infra/providers.tf
+++ .infra/infra/providers.tf
@@ -13,3 +13,10 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
   token      = ""
 }
+
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+  token      = ""
+}
--- .infra/infra/testing/terraform.tf
+++ .infra/infra/testing/terraform.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    eu-central-1 = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
