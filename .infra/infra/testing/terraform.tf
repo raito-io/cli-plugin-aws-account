acc3d6561a6e2ab20521ddbdbd35e711334cbcda
Dieter Wachters <dieter@raito.io>
2024-05-15 11:51:39 +0200

    old provider back

--- .infra/infra/testing/terraform.tf
+++ .infra/infra/testing/terraform.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    eu-central-1 = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
