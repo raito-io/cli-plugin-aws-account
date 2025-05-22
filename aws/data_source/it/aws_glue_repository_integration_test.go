5593c2c8ab1d814ff9e31796c74fdd71a7aa47a4
Dieter Wachters <dieter@raito.io>
2025-05-22 16:22:20 +0200

    Fixing tests

--- aws/data_source/it/aws_glue_repository_integration_test.go
+++ aws/data_source/it/aws_glue_repository_integration_test.go
@@ -73,7 +73,7 @@ func (s *GlueRepositoryTestSuite) TestGlueRepository_FetchTest() {
 	}
 }
 
-func (s *GlueRepositoryTestSuite) TestGlueRepository_FetchTest_WithExclusions() {
+func (s *GlueRepositoryTestSuite) TestGlueRepository_FetchTest_WithExclusions_Tables() {
 	config := s.GetConfig()
 	syncer := data_source.NewDataSourceSyncer()
 	dsHandler := DummyDataSourceHandler{}
@@ -81,7 +81,6 @@ func (s *GlueRepositoryTestSuite) TestGlueRepository_FetchTest_WithExclusions() 
 	config.Parameters[constants.AwsGlueEnabled] = "true"
 	config.Parameters[constants.AwsS3Enabled] = "false"
 	config.Parameters[constants.AwsRegions] = "eu-central-1,eu-west-1"
-	config.Parameters[constants.AwsGlueExcludeDatabases] = "raito-data-corporate"
 	config.Parameters[constants.AwsGlueExcludeTables] = "car.*"
 
 	err := syncer.SyncDataSource(context.Background(), &dsHandler, &ds2.DataSourceSyncConfig{ConfigMap: config})
@@ -92,10 +91,51 @@ func (s *GlueRepositoryTestSuite) TestGlueRepository_FetchTest_WithExclusions() 
 
 	doMap := map[string]string{
 		account: "datasource",
-		// raito-data-corporate database in eu-central-1 is excluded
-		"077954824694:eu-west-1:raito-data-west": "bucket",
-		// cars table in raito-data-west database in eu-west-1 is excluded
-		"077954824694:eu-west-1:raito-data-west/operations": "glue-table",
+		"077954824694:eu-central-1:raito-data-corporate":            "bucket",
+		"077954824694:eu-central-1:raito-data-corporate/operations": "glue-table",
+		"077954824694:eu-central-1:raito-data-corporate/marketing":  "glue-table",
+		"077954824694:eu-central-1:raito-data-corporate/sales":      "glue-table",
+		"077954824694:eu-west-1:raito-data-west":                    "bucket",
+		"077954824694:eu-west-1:raito-data-west/operations":         "glue-table",
+	}
+
+	dataObjectmap := make(map[string]string)
+
+	fetchedDOs := []*ds2.DataObject{}
+
+	for _, do := range dsHandler.DataObjects {
+		if do.Type != ds2.Column { // We ignore columns for this test
+			dataObjectmap[do.FullName] = do.Type
+			fetchedDOs = append(fetchedDOs, do)
+		}
+	}
+
+	s.Require().Lenf(fetchedDOs, len(doMap), "Expected %d data objects, got %d: %+v != %+v", len(doMap), len(fetchedDOs), doMap, dataObjectmap)
+
+	for _, do := range fetchedDOs {
+		s.Require().Contains(doMap, do.FullName)
+		s.Require().Equal(doMap[do.FullName], do.Type)
+	}
+}
+
+func (s *GlueRepositoryTestSuite) TestGlueRepository_FetchTest_WithExclusions_Databases() {
+	config := s.GetConfig()
+	syncer := data_source.NewDataSourceSyncer()
+	dsHandler := DummyDataSourceHandler{}
+
+	config.Parameters[constants.AwsGlueEnabled] = "true"
+	config.Parameters[constants.AwsS3Enabled] = "false"
+	config.Parameters[constants.AwsRegions] = "eu-central-1,eu-west-1"
+	config.Parameters[constants.AwsGlueExcludeDatabases] = "raito_catalog.*"
+
+	err := syncer.SyncDataSource(context.Background(), &dsHandler, &ds2.DataSourceSyncConfig{ConfigMap: config})
+
+	s.Require().NoError(err)
+
+	account, _ := repo.GetAccountId(context.Background(), config)
+
+	doMap := map[string]string{
+		account: "datasource",
 	}
 
 	dataObjectmap := make(map[string]string)
